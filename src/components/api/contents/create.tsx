bd986f171994da44f79c393d93e212df1eb79656
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2022-01-14 09:08:02 +0900

    画像プレビュー表示の実装。とりあえず表示できるだけ。TODO: サイズとか、書き方とか改善する

--- src/components/api/contents/create.tsx
+++ src/components/api/contents/create.tsx
@@ -1,13 +1,19 @@
+import { builtinModules } from 'module'
 import { urlHost } from '../common'
 
 type contentProps = {
 	title: string,
 	description: string,
-	image: string
+	image: HTMLInputElement | null
 }
 
 const CreateContent = async (props: contentProps) => {
-	const data: Object = {
+	const body = new FormData()
+	body.append("title", props.title)
+	body.append("description", props.description)
+	// body.append("image", props.image?.formTarget)
+
+	const data: RequestInit = {
 		method: 'POST',
 		mode: 'cors',
 		// credentials: 'include',
@@ -15,13 +21,16 @@ const CreateContent = async (props: contentProps) => {
 			'Access-Control-Request-Method': 'POST',
 			'Access-Control-Request-Headers': 'origin, content-type, accept, access-control-request-method',
 			'Origin': "http://localhost:3000",
-			'Content-Type': 'application/json',
-			'Accept': 'application/json'
+			// 'Content-Type': 'application/json',
+			// 'Accept': 'application/json',
+			'Content-Type': 'multipart/form-data',
+			'Accept': 'multipart/form-data',
 		},
 		body: JSON.stringify(props)
 	}
+	// delete data.headers['Content-Type']
 
-console.log(data)
+	console.log(data)
 	const response = await fetch(urlHost + 'contents', data)
 	.then((response) => {return (response.json()).then((data) => {return data})}).catch((error) => {
 		console.log(error)
--- src/components/pages/contents/new.tsx
+++ src/components/pages/contents/new.tsx
@@ -1,11 +1,16 @@
 import Header from '../../commons/header'
 import CreateContent from '../../api/contents/create'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import { jsx, css } from '@emotion/react'
+
+const contentStyle = css({
+	color: 'white',
+})
 
 type contentProps = {
 	title: string,
 	description: string,
-	image: string
+	image: HTMLInputElement | null
 }
 
 const CreateContentRequest = (props: contentProps) => {
@@ -14,19 +19,42 @@ const CreateContentRequest = (props: contentProps) => {
 	})
 }
 
+const previewImage = (files: FileList | null) => {
+	if (files === null) {
+		return
+	}
+
+
+	// readerのresultプロパティに、データURLとしてエンコードされたファイルデータを格納
+	var reader = new FileReader();
+	console.log(files[0])
+	return URL.createObjectURL(files[0])
+	// const file = reader.readAsDataURL(files[0]);
+
+}
+
 
 const NewContents = () => {
-	const [title, setTitle] = useState("test title")
-	const [description, setDescription] = useState("test desript")
-	const [image, setImage] = useState("")
+	const [title, setTitle] = useState<string>("test title")
+	const [description, setDescription] = useState<string>("test desript")
+
+	// プレビュー表示用
+	const [image, setImage] = useState<string>("")
+
+	const fileInput = useRef<HTMLInputElement>(null);
 
 	return (
 		<div>
 			<Header />
 			<div>
-				<input type="text" id="title" name="title" placeholder='タイトル' value='test title' onChange={(e) => {setTitle(e.target.value)}} />
-				<input type="text" id="description" name="description" placeholder='詳細' value='test description' onChange={(e) => {setDescription(e.target.value)}}/>
-				<input type="submit" value='登録' onClick={() => CreateContentRequest({title: title, description: description, image: image})}/>
+				<input type="text" id="title" name="title" placeholder='タイトル' value={title} onChange={(e) => {setTitle(e.target.value)}} />
+				<input type="text" id="description" name="description" placeholder='詳細' value={description} onChange={(e) => {setImage(e.target.value)}}/>
+				<input type="file" name="image" accept="image/*" ref={fileInput} onChange={(e) => {const url: string | undefined = previewImage(e.target.files); if (url != undefined) {setImage(url)}}}/>
+				<input type="submit" value='登録' onClick={() => CreateContentRequest({title: title, description: description, image: fileInput.current})}/>
+			</div>
+			<div>
+				<div css={contentStyle}>{image}</div>
+				<img src={image}></img>
 			</div>
 		</div>
 	)
